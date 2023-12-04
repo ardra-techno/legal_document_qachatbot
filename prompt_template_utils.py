f95c3995b0ce4683dbaaa9532ecc077fc7f99a91
ardra-techno <138871618+ardra-techno@users.noreply.github.com>
2023-12-04 13:04:41 +0530

    Update prompt_template_utils.py

--- prompt_template_utils.py
+++ prompt_template_utils.py
@@ -9,9 +9,10 @@ from langchain.prompts import PromptTemplate
 
 # this is specific to Llama-2.
 
-system_prompt = """You are a helpful assistant, you will use the provided context to answer user questions.
-Read the given context before answering questions and think step by step. If you can not answer a user question based on 
-the provided context, inform the user. Do not use any other information for answering user. Provide a detailed answer to the question."""
+system_prompt = """As a legal expert chatbot specializing in Australian law, you are engaging in a conversation with a human user. 
+Please carefully read the provided context before responding to questions and approach each inquiry step by step. 
+Some time you cannot find any data in document. that time Utilize your knowledge of Australian legislation exclusively to address the user's queries. 
+Avoid incorporating information from sources outside Australian law. Provide a comprehensive and detailed response to each question."""
 
 
 def get_prompt_template(system_prompt=system_prompt, promptTemplate_type=None, history=False):
